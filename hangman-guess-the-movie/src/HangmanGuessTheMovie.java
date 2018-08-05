e7a02613b6a9b56deb5626c0fb200fbe545f867d
KatSaldivar <katherinekaufmann1@gmail.com>
2018-08-05 10:13:51 -0700

    included more output and started on game loop logic

--- hangman-guess-the-movie/src/HangmanGuessTheMovie.java
+++ hangman-guess-the-movie/src/HangmanGuessTheMovie.java
@@ -1,13 +1,46 @@
+import java.awt.*;
 import java.util.*;
 import java.io.*;
 
 public class HangmanGuessTheMovie {
     public static void main (String [] args) throws Exception {
         boolean hasWon = false;
-        System.out.println("I have randomly  chosen a movie title.  Try to guess it.");
+        System.out.println("I have randomly chosen a movie title.  Try to guess without reaching 10 incorrect guesses.");
+        String movieTitle = null;
+        StringBuilder correctGuessTitleOutput =new StringBuilder();
+        StringBuilder [] incorrectGuesses = new StringBuilder[10];
+        Scanner scanner = new Scanner(System.in);
 
-        String movieTitle = selectMovieTitle();
-        System.out.println(movieTitle);
+        try{
+             movieTitle = selectMovieTitle();
+        }catch (Exception exception){
+            System.out.println("Oops! Looks like the movie file is missing.");
+        }
+
+        //SETS INITIAL TITLE OUTPUT TO BE BLANK
+        for (int i=0; i < movieTitle.length(); i++ ){
+            if (movieTitle.charAt(i)!= ' '){
+                correctGuessTitleOutput.insert(i, '_').append(" ");
+            }else {
+                correctGuessTitleOutput.insert(i, ' ');
+            }
+        }
+        //TODO add space between the _ to see how many char to guess
+        //StringBuilder newcorrectGuessTitleOutput = correctGuessTitleOutput.replaceAll(".(?=.)", "$0 ");//correctGuessTitleOutput.replace("", " ").trim()//correctGuessTitleOutput.replaceAll(".(?=.)", "$0 ");
+        //correctGuessTitleOutput.replace(i,i,"_ ");//correctGuessTitleOutput.insert(i, '_');
+
+        System.out.println(movieTitle + correctGuessTitleOutput);
+        for (int i = 10; i > 0; i--) {
+            System.out.println("You have guessed these wrong letters: " + incorrectGuesses);
+            System.out.println("You have " + i + " guesses left. What is your next guess? ");
+            String currentLetterGuess = scanner.nextLine();
+            System.out.println("Your guess was: " + currentLetterGuess);
+        }
+        if (hasWon) {
+            System.out.println("Correct! The movie title was: " + movieTitle + " You win!");
+        }else{
+            System.out.println("Game over! You lose.  The movie title was " + movieTitle + ".");
+        }
 
         /*for (int i = 10; i > 0; i--) {
             System.out.println("You are guessing:" + );
@@ -48,7 +81,7 @@ public class HangmanGuessTheMovie {
             scanner.nextLine();
         }
 
-
+        //SELECTS A RANDOM LINE FROM FILE
         int randomline = (int) (Math.random() * lines) + 1;
         FileInputStream fs = new FileInputStream("movies.txt");
         BufferedReader br = new BufferedReader(new InputStreamReader(fs));
